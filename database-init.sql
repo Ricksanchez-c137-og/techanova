adc107b7e9b448f1b2d9d709d0df184f7f9fd363
Ricksanchez-c137-og <sobermanisgod@gmail.com>
2024-12-03 20:02:44 +0400

    modified database-init.sql

--- database-init.sql
+++ database-init.sql
@@ -1,17 +1,19 @@
 CREATE DATABASE IF NOT EXISTS contactdb;
+
 USE contactdb;
 
 CREATE TABLE IF NOT EXISTS messages (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    email VARCHAR(255),
-    subject VARCHAR(255),
-    message TEXT
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(255) NOT NULL,       
+    email VARCHAR(255) NOT NULL,      
+    subject VARCHAR(255),            
+    message TEXT NOT NULL,            
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP 
 );
 
 CREATE TABLE IF NOT EXISTS flags (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    flag VARCHAR(255)
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    flag VARCHAR(255) NOT NULL         
 );
 
 INSERT INTO flags (flag) VALUES ("Al-Hosn{sql_injection_success}");
